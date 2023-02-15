cc26c1bfb1450d3bd568f23aabd12c783ded08af
zyangchuan <120377009+zyangchuan@users.noreply.github.com>
2023-02-16 00:15:16 +0800

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,30 @@
 <template>
   <v-app-bar app size="large">
-    <v-app-bar-nav-icon color="white" v-on:click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-app-bar-nav-icon
+      color="white"
+      v-on:click="drawer = !drawer"
+    ></v-app-bar-nav-icon>
     <v-app-bar-title>
-      <span class="font-weight-light text-body-1 text-sm-h6">FlashLearn</span>
+      <img
+        :src="require('@/assets/flashlearn-text-logo.svg')"
+        style="width: 100px"
+        class="hidden-sm-and-down mt-1"
+      />
     </v-app-bar-title>
     <v-spacer></v-spacer>
-    <p class="h6 mx-6 font-weight-medium hidden-sm-and-down">Hello {{ username }}</p>
-    <v-btn 
+    <p class="h6 mx-6 font-weight-medium hidden-sm-and-down">
+      Hello {{ username }}
+    </p>
+    <!-- <v-btn 
       variant="flat" 
       prepend-icon="mdi-comment-quote-outline"
-      class="text-capitalize">Feedback</v-btn>
-    <v-btn 
+      class="text-capitalize">Feedback</v-btn> -->
+    <v-btn
       color="white"
       prepend-icon="mdi-exit-to-app"
       :loading="signOutLoading"
       v-on:click="signOut"
+      class="text-capitalize"
     >
       Sign out
     </v-btn>
@@ -27,24 +37,21 @@
     </div>
 
     <v-list>
-      <v-list-item 
+      <v-list-item
         prepend-icon="mdi-cards"
-        title="Card decks" 
+        title="Card decks"
         value="carddecks"
         v-bind:to="{ name: 'CardDecks' }"
         active-color="primary"
       >
       </v-list-item>
 
-      <v-list-item 
-        prepend-icon="mdi-account-multiple"
-        value="friends"
-        disabled>
+      <v-list-item prepend-icon="mdi-account-multiple" value="friends" disabled>
         <v-list-item-title class="d-inline">Friends</v-list-item-title>
-        <v-chip class="mx-4"  size="small">Coming soon</v-chip>
+        <v-chip class="mx-4" size="small">Coming soon</v-chip>
       </v-list-item>
 
-      <v-list-item 
+      <v-list-item
         prepend-icon="mdi-account"
         title="Account"
         value="accounts"
@@ -57,38 +64,38 @@
 </template>
 
 <script>
-import { ref } from '@vue/reactivity'
-import { watchEffect } from '@vue/runtime-core'
-import { useStore } from 'vuex'
-import { Auth } from "aws-amplify"
+import { ref } from "@vue/reactivity";
+import { watchEffect } from "@vue/runtime-core";
+import { useStore } from "vuex";
+import { Auth } from "aws-amplify";
 
 export default {
   setup() {
-    const store = useStore()
-    
-    const drawer = ref(false)
-    const username = ref("")
+    const store = useStore();
+
+    const drawer = ref(false);
+    const username = ref("");
 
     watchEffect(() => {
-      const user = store.state.currentUser
-      username.value = user.attributes.preferred_username
-    })
+      const user = store.state.currentUser;
+      username.value = user.attributes.preferred_username;
+    });
 
-    const signOutLoading = ref(false)
+    const signOutLoading = ref(false);
     const signOut = () => {
-      signOutLoading.value = true
+      signOutLoading.value = true;
       Auth.signOut()
         .then(() => {
-          store.commit("setCurrentUser", null)
-          signOutLoading.value = false
+          store.commit("setCurrentUser", null);
+          signOutLoading.value = false;
         })
-        .catch(error => {
-          console.log(error)
-          signOutLoading.value = false
-        })
-    }
+        .catch((error) => {
+          console.log(error);
+          signOutLoading.value = false;
+        });
+    };
 
-    return { drawer, username, signOutLoading, signOut }
-  }
-}
+    return { drawer, username, signOutLoading, signOut };
+  },
+};
 </script>
